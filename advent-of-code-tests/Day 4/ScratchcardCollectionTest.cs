55a84cbab5cd5fccef20373485d1d0bef4711d97
Simon <simon.vereecke@live.com>
2023-12-26 21:53:14 +0100

    Scratchcard Part Two - Complete

--- advent-of-code-tests/Day 4/ScratchcardCollectionTest.cs
+++ advent-of-code-tests/Day 4/ScratchcardCollectionTest.cs
@@ -22,17 +22,5 @@ namespace advent_of_code_tests.Day_4
 
             Assert.Equal(30, collection.Total);
         }
-
-        [Fact]
-        public void Total_ReturnsSumOfFirstAndSecond()
-        {
-            ScratchcardCollection collection = new([
-                new Scratchcard([41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53]),
-                new Scratchcard([13, 32, 20, 16, 61], [61, 30, 68, 82, 17, 32, 24, 19]),
-                new Scratchcard([1, 21, 53, 59, 44], [69, 82, 63, 72, 16, 21, 14, 1]),
-                new Scratchcard([41, 92, 73, 84, 69], [59, 84, 76, 51, 58, 5, 54, 83])
-            ]);
-            Assert.Equal(15, collection.Total);
-        }
     }
 }
--- advent-of-code/Day 4/ScratchcardCollection.cs
+++ advent-of-code/Day 4/ScratchcardCollection.cs
@@ -2,72 +2,56 @@
 {
     public class ScratchcardCollection(Scratchcard[] scratchcards)
     {
-        //private ScratchcardCounter[] Scratchcards { get; } = scratchcards.Select(card => new ScratchcardCounter(card)).ToArray();
-
-        //private Dictionary<int, Scratchcard[]> Scratchcards { get; } = scratchcards.Select((card, index) => new { ScratchCard = card, Index = index }).ToDictionary(x => x.Index, x => new Scratchcard[] { x.ScratchCard });
-
-        private List<Scratchcard> Scratchcards = new List<Scratchcard>(scratchcards);
+        private readonly List<Scratchcard> Scratchcards = new(scratchcards);
+        private readonly Dictionary<int, ScratchcardCounter> Counters = [];
 
         public int Total => GetTotal();
 
         private int GetTotal()
         {
-            int total = 0;
+            for (int i = 0; i < Scratchcards.Count; i++)
+            {
+                Counters.Add(i, new(Scratchcards[i]));
+            }
 
-            //while(Scratchcards.Count > 0)
-            //{
-            //    total++;
-            //    Scratchcard scratchcard = Scratchcards.First();
-            //    int matchingNumbers = scratchcard.MatchingNumbers;
-            //    for (int i = 1; i < matchingNumbers; i++)
-            //    {
-            //        Scratchcards.Add(Scratchcards[i]);
-            //    }
-            //    Scratchcards.Remove(scratchcard);
-            //}
 
-            //for (int i = 0; i < Scratchcards.Length; i++)
-            //{
-            //    for (int j = 0; j < Scratchcards[i].Amount; j++)
-            //    {
-            //        total++;
-            //        int matchingNumbers = Scratchcards[i].MatchingNumbers;
+            for (int i = 0; i < Counters.Count; i++)
+            {
+                var counter = Counters[i];
 
-            //        for (int h = 1; h < matchingNumbers; h++)
-            //        {
-            //            if ((i + h) < Scratchcards.Length)
-            //                Scratchcards[i + h].AddCard();
-            //        }
-            //    }
-            //}
+                while (counter.Balance > 0)
+                {
+                    int matchingNumbers = counter.MatchingNumbers;
+                    for (int j = 1; j <= matchingNumbers; j++)
+                    {
+                        if(i + j < Counters.Count)
+                        {
+                            Counters[i + j].AddCard();
+                        }
+                    }
+                    counter.RemoveCard();
+                }
+            }
 
-            //for (int i = 0; i < Scratchcards.Count; i++)
-            //{
-            //    foreach (Scratchcard scratchcard in Scratchcards[i])
-            //    {
-            //        total++;
-            //        int matchingNumbers = scratchcard.MatchingNumbers;
-            //        for (int j = 1; j < matchingNumbers; j++)
-            //        {
-            //            Scratchcards[j] = [.. Scratchcards[j], Scratchcards[j].First()];
-            //        }
-            //    }
-            //}
-
-            return total;
+            return Counters.Sum(counter => counter.Value.Total);
         }
 
         private class ScratchcardCounter(Scratchcard scratchcard)
         {
-            public int Amount = 1;
-            //private Scratchcard Scratchcard { get; } = scratchcard;
+            public int Balance = 1;
+            public int Total = 1;
 
             public int MatchingNumbers { get; } = scratchcard.MatchingNumbers;
 
             public void AddCard()
             {
-                Amount++;
-                //return this;
+                Total++;
+                Balance++;
+            }
+
+            public void RemoveCard()
+            {
+                Balance--;
             }
         }
     }
