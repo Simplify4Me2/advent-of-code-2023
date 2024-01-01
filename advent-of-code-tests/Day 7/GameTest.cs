7d68fe60fdaa1af2fb0bcaee2d0a4bf1ed1e10af
Simon <simon.vereecke@live.com>
2024-01-01 21:34:33 +0100

    update fact to theory

--- advent-of-code-tests/Day 7/GameTest.cs
+++ advent-of-code-tests/Day 7/GameTest.cs
@@ -1,6 +1,27 @@
 ﻿using advent_of_code.Day_7;
+using System.Collections;
 
 namespace advent_of_code_tests.Day_7;
+
+public class GameDataGenerator : IEnumerable<object[]>
+{
+    private readonly List<object[]> _data =
+    [
+        [new Hand[] 
+        {
+            new Hand(['3', '2', 'T', '3', 'K'], 765),
+            new Hand(['T', '5', '5', 'J', '5'], 684),
+            new Hand(['K', 'K', '6', '7', '7'], 28),
+            new Hand(['K', 'T', 'J', 'J', 'T'], 220),
+            new Hand(['Q', 'Q', 'Q', 'J', 'A'], 483)
+        },
+        6440]
+    ];
+
+    public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
 
 public class GameTest
 {
@@ -12,11 +33,12 @@ public class GameTest
         new Hand(['Q', 'Q', 'Q', 'J', 'A'], 483),
     ];
 
-    [Fact]
-    public void DetermineTotalWinnings_ReturnsSumofEachHandsBidMultipliedWithItsRank()
+    [Theory]
+    [ClassData(typeof(GameDataGenerator))]
+    public void DetermineTotalWinnings_ReturnsSumofEachHandsBidMultipliedWithItsRank(Hand[] hands, int expectedWinnings)
     {
         Game game = new(hands);
 
-        Assert.Equal(6440, game.TotalWinnings);
+        Assert.Equal(expectedWinnings, game.TotalWinnings);
     }
 }
